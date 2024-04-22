325cb6adc1f0186df506bfe2bbd6c81eea0c8750
mish9408@gmail.com <mish9408@gmail.com>
2024-04-22 17:37:33 +0200

    Update javascript code

--- JavaS.js
+++ JavaS.js
@@ -1,21 +1,18 @@
 
 /*global $,document*/
 /*eslint-disable no-console*/
-// Your name as a string
+
 
 const myName = "Mishael";
 const nameSurname = "Mishael Ntsoane"
-console.log(myName);
-console.log(nameSurname);
-
-// Select the HTML element by its ID
-const nameContainer = document.getElementById("name");
-
-const nameSur = document.getElementsId("nameSurname");
 
 
-// Display the name in the element
+
+
+const nameContainer = document.getElementById('name');
+const surnameContainer = document.getElementById('namesurname');
+
 nameContainer.textContent = myName;
-nameSur.textContent = nameSurname;
+surnameContainer.textContent = mySurname;
 
 
